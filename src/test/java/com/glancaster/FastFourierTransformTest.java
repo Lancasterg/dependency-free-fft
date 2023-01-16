dec41ec98e10969c89bbc3104c5f35906f2faad3
lancasterg <lancaster0180@gmail.com>
2023-01-16 16:34:16 +0000

    Comment out invalid test

--- src/test/java/com/glancaster/FastFourierTransformTest.java
+++ src/test/java/com/glancaster/FastFourierTransformTest.java
@@ -10,27 +10,28 @@ public class FastFourierTransformTest {
     
     @Test
     public void testFFT() {
-        Complex input[] = {cplx_1_1, cplx_1_1, cplx_1_1, cplx_1_1, cplx_0_0, cplx_0_0, cplx_0_0, cplx_0_0};
-        Complex truth[] = {
-            new Complex(4, 0), 
-            new Complex(1, -2.414214),
-            new Complex(0, 0),
-            new Complex(1, -0.414214),
-            new Complex(0, 0),
-            new Complex(1, 0.414214),
-            new Complex(0, 0),
-            new Complex(1, 2.414214),
-        };
+        // TODO: finish this
+        // Complex input[] = {cplx_1_1, cplx_1_1, cplx_1_1, cplx_1_1, cplx_0_0, cplx_0_0, cplx_0_0, cplx_0_0};
+        // Complex truth[] = {
+        //     new Complex(4, 0), 
+        //     new Complex(1, -2.414214),
+        //     new Complex(0, 0),
+        //     new Complex(1, -0.414214),
+        //     new Complex(0, 0),
+        //     new Complex(1, 0.414214),
+        //     new Complex(0, 0),
+        //     new Complex(1, 2.414214),
+        // };
 
-        Complex output[];
+        // Complex output[];
 
-        FastFourierTransform FFT = new FastFourierTransform();
-        output = FFT.fft(input);
+        // FastFourierTransform FFT = new FastFourierTransform();
+        // output = FFT.fft(input);
 
-        for (int i = 0; i < input.length; i++) {
-            assertEquals(truth[i].getRealPart(), output[i].getRealPart(), 0.01);
-            assertEquals(truth[i].getImagPart(), output[i].getImagPart(), 0.01);
-        }
+        // for (int i = 0; i < input.length; i++) {
+        //     assertEquals(truth[i].getRealPart(), output[i].getRealPart(), 0.01);
+        //     assertEquals(truth[i].getImagPart(), output[i].getImagPart(), 0.01);
+        // }
     }
 
     @Test
